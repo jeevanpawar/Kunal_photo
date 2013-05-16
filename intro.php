8c08e975c2b56654978ef1ad942860b51ab9faf8
Jeevan Gulab Pawar <jeevan.wavetechline@gmail.com>
2013-05-16 17:52:34 +0530

    introduction

---
+++ intro.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body oncontextmenu="return false;">
+<table width="100%" border="0" cellspacing="1" cellpadding="1">
+<tr>
+<td>&nbsp;</td>
+<td><br>
+<br>
+<br>
+<br>
+</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td><div align="center">
+<OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
+codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
+WIDTH="700" HEIGHT="350" ALIGN="middle">
+<PARAM NAME=movie VALUE="yourintro.swf">
+<PARAM NAME=quality VALUE=high>
+<PARAM NAME=bgcolor VALUE=#FFFFFF>
+<EMBED src="whitebird1.swf" quality=high bgcolor=#FFFFFF WIDTH="700" HEIGHT="350" ALIGN="middle"
+TYPE="application/x-shockwave-flash" PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer"></EMBED> 
+</OBJECT>
+</div></td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>
+<table width="700" border="0" align="center" cellpadding="1" cellspacing="1">
+<tr>
+<td align="right"><a href="index.php">Login</a></td>
+</tr>
+</table>
+</td>
+<td>&nbsp;</td>
+</tr>
+</table>
+</body>
+</html>
